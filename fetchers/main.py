e58131c6fa0541d948b8972bfac69e5b5b446ca2
9ardenx <9ardenx@hufs.ac.kr>
2025-06-28 16:05:10 +0900

    Create main.py

---
+++ fetchers/main.py
@@ -0,0 +1,17 @@
+import itertools, asyncio
+from orders.smartstore_orders import fetch_orders as ss_fetch
+from orders.coupang_orders import fetch_orders as cp_fetch
+from invoices.builder import build_invoices
+from exporter.csv_exporter import save_csv
+from notifier.kakao import send_file_link     # TODO: 링크 함수 완성
+
+async def run():
+    ss, cp = await asyncio.gather(ss_fetch(), cp_fetch())
+    orders = ss + cp
+    invoices = list(itertools.chain.from_iterable(build_invoices(o) for o in orders))
+    csv_path = save_csv(invoices)
+    # csv_path를 S3/Render static에 업로드 → URL
+    # send_file_link(url)
+
+if __name__ == '__main__':
+    asyncio.run(run())
