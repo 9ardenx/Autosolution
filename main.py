26065176407c78ff28dedd66fe2b914b5b338443
9ardenx <9ardenx@hufs.ac.kr>
2025-06-28 18:05:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fetchers.coupang import fetch_orders as cp_fetch
 from invoices.builder import build_invoices
 from exporter.csv_exporter import save_csv
 from notifier.kakao import send_file_link     # TODO: 링크 함수 완성
+from dotenv import load_dotenv
+load_dotenv()               # .env 값을 환경변수로 주입
 
 async def run():
     ss, cp = await asyncio.gather(ss_fetch(), cp_fetch())
